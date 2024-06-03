c4e736df6c4515b87195023ef61c17fcabdc2964
rzcastilho <rodrigo.zampieri@gmail.com>
2024-06-03 18:07:33 -0300

    feat: :sparkles: dashboard details

--- lib/our_expenses_web/live/dashboard_live/index.ex
+++ lib/our_expenses_web/live/dashboard_live/index.ex
@@ -2,6 +2,7 @@ defmodule OurExpensesWeb.DashboardLive.Index do
   use OurExpensesWeb, :live_view
 
   alias OurExpenses.Expenses
+  alias OurExpenses.Expenses.Category
 
   @impl true
   def mount(_params, _session, socket) do
@@ -16,4 +17,18 @@ defmodule OurExpensesWeb.DashboardLive.Index do
       |> assign(:balance_grouped_by_category, Expenses.balance_grouped_by_category(bill))
     }
   end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :details, %{"category_id" => category_id}) do
+    with %Category{} = category <- Expenses.get_category!(category_id) do
+      socket
+      |> assign(:category, category)
+    end
+  end
+
+  defp apply_action(socket, :index, _params), do: socket
 end
