83844524f7c88f8460347569796ae4acbfcbbffd
rzcastilho <rodrigo.zampieri@gmail.com>
2024-09-11 00:18:14 -0300

    feat: :recycle: convert database from postgresql to sqlite3

--- lib/our_expenses_web/release.ex
+++ lib/our_expenses_web/release.ex
@@ -3,12 +3,28 @@ defmodule OurExpensesWeb.Release do
   Used for executing DB release tasks when run in production without Mix
   installed.
   """
+  require Logger
+
   @app :our_expenses
 
   def migrate do
     load_app()
 
     for repo <- repos() do
+      case repo.__adapter__().storage_up(repo.config()) do
+        :ok ->
+          Logger.info("The database for #{inspect(repo)} has been created")
+
+        {:error, :already_up} ->
+          Logger.info("The database for #{inspect(repo)} has already been created")
+
+        {:error, term} when is_binary(term) ->
+          Logger.error("The database for #{inspect(repo)} couldn't be created: #{term}")
+
+        {:error, term} ->
+          Logger.error("The database for #{inspect(repo)} couldn't be created: #{inspect(term)}")
+      end
+
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
