ae70dfe11338a543015b03ad3d43a6d333249fa7
rzcastilho <rodrigo.zampieri@gmail.com>
2024-09-10 23:47:02 -0300

    feat: :recycle: convert database from postgresql to sqlite3

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,7 +30,7 @@ if config_env() == :prod do
 
   config :our_expenses, OurExpenses.Repo,
     # ssl: true,
-    url: database_url,
+    database: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
